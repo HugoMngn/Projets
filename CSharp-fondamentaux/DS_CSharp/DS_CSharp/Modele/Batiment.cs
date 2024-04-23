fa1df071f2a9ede5bbea452aadd43ea072740248
HugoMngn <166018262+HugoMngn@users.noreply.github.com>
2024-04-23 12:17:21 +0200

    Add files via upload

---
+++ CSharp-fondamentaux/DS_CSharp/DS_CSharp/Modele/Batiment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Salle-Hugo.Modele
+{
+    public class Batiment
+    {
+        public string Code { get; set; }
+        public string Nom { get; set; }
+        public int Adresse { get; set; }
+        public int CodePostal { get; set; }
+        public string Ville { get; set; }
+    }
+}
